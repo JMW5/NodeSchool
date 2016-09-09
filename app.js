6c4eacd80cca9f5dda78db85479df4f351c15f38
Jac <jacquinwynn91@gmail.com>
2016-09-08 23:11:17 -0400

    app change

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(5000, function(){
+app.listen(process.env.PORT || 5000, function(){
   console.log("Server is running on Port 5000, Enjoy!");
 })
 
